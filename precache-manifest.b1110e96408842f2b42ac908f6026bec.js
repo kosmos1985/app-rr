e6d96b309a315b75b57583b87202f30e4f2f3805
Maciej Kosmela <alternatywnykosmos@gmail.com>
2020-06-30 13:55:46 +0200

    Updates

---
+++ precache-manifest.b1110e96408842f2b42ac908f6026bec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ac66872115b43bba6e",
+    "url": "/app-rr/static/css/main.19e414cf.chunk.css"
+  },
+  {
+    "revision": "99ac66872115b43bba6e",
+    "url": "/app-rr/static/js/main.99ac6687.chunk.js"
+  },
+  {
+    "revision": "9873500cbc742282ea50",
+    "url": "/app-rr/static/js/runtime~main.9873500c.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/app-rr/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "c10691a0439fd6ca9bb560e62a178918",
+    "url": "/app-rr/index.html"
+  }
+];
